646a1d6e408c224f65f6ef6e3b2686a4370dcf12
CharaD7 <jijakahn6@gmail.com>
2023-05-03 06:24:02 +0000

    Added install prerequisite installs for dwm

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Install prerequisites for Dynamic Window Manager (dwm)
+sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
