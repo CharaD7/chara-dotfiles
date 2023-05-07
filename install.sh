97037348f1a5939406f8e90670cfa0c96c629be5
CharaD7 <jijakahn6@gmail.com>
2023-05-07 19:00:43 +0000

    Addeed command to update system and install latest dwm

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# do a global system update
+sudo apt update && sudo apt upgrade
+
 # Install prerequisites for Dynamic Window Manager (dwm)
 sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
 sudo apt install build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
+
+# Install dwm
+sudo apt install dwm
