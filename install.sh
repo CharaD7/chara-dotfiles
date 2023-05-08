bb3310ac51a8762f8f0a6e3bb7c43307b94f92f1
CharaD7 <jijakahn6@gmail.com>
2023-05-08 14:04:55 +0000

    Added config steps for git

--- install.sh
+++ install.sh
@@ -1,31 +1,47 @@
 #!/bin/sh
 
+# Stop on errors
+set -e
+
 # do a global system update
-echo "Running system update"
+echo "Running system update..."
 sudo apt update && sudo apt upgrade
 
 # Install prerequisites for Dynamic Window Manager (dwm)
-echo "Installing prerequisites"
+echo "Installing prerequisites..."
 sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
-sudo apt install build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm git
+sudo apt install build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
 
-# Install dwm
-echo "Installing dwm"
-sudo apt install dwm
+echo "Checking for git installation..."
+gitPath = $(which git)
+# Checking to see if git path is registered
+if [ $gitPath -ne "/usr/bin/git" ]; then
+  echo "Git not detected, installing git..."
+  sudo apt install git
+fi
 
-# Install gitconfig with alias
+# Setting up gitconfig with alias
 read -p "Would you like to setup your gitconfig now? [Y,n]: " -i Y gitConfigReply
-if [ "$gitConfigReply" = "y" || "$gitConfigReply" = "Y" ]; then
+if [ $gitConfigReply = "y" || $gitConfigReply = "Y" ]; then
   read -r "Enter your git username: " gitUsername
   read -r "Enter your git email address: " gitEmail
 
+  echo "Setting your global git username to '$gitUsername'"
   git config --global user.name "$gitUsername"
+  echo "Setting your global git email to '$gitEmail'"
   git config --global user.email "$gitEmail"
 
 else
-  echo "Skipping git configuration. Kindly configure your git after installation process"
+  echo "Skipping git configuration. Kindly configure your git after installation process..."
 fi
+
+cp -r .gitconfig ~/.gitconfig
 
 alias g=git
 sleep 1
 echo "Your git command is now aliased 'g'"
+echo "You can run 'g cn' to check your global git username and 'g ce' to check your global git email address."
+
+# Install dwm
+echo "Installing dwm..."
+sudo apt install dwm
