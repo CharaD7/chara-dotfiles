3aa2b7b9b1ad4c837b55b326d32ad504177c81fd
CharaD7 <jijakahn6@gmail.com>
2023-05-24 19:13:47 +0000

    Refactored install script

--- install.sh
+++ install.sh
@@ -202,6 +202,9 @@ setNeovideConfig() {
 
 # DWM config setup function
 setDWMConfig() {
+  echo "Last step!"
+  # Install dwm
+
   # Install prerequisites for Dynamic Window Manager (dwm)
   echo "Installing prerequisites for dwm..."
   sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
@@ -274,8 +277,3 @@ setNeovideConfig
 
 # Do dwm config task
 setDWMConfig
-
-
-echo "Last step!"
-# Install dwm
-configDwm &
