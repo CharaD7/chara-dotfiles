e9832715c853fd38208d532f455d45c95b7cd6e3
CharaD7 <jijakahn6@gmail.com>
2023-09-30 08:47:14 +0000

    Included session map looks

--- nvim/after/plugin/session-lens.lua
+++ nvim/after/plugin/session-lens.lua
@@ -1,9 +1,16 @@
 ---@diagnostic disable: undefined-global
+local status_tel_ok, telescope = pcall(require, 'telescope')
+if not status_tel_ok then return end
+
 local status_ok, session_lens = pcall(require, 'session-lens')
 if not status_ok then return end
 
+telescope.load_extension(session_lens)
+
 session_lens.setup {
+  theme = 'ivy',
+  theme_config = { bordered = true },
   path_display = { "shorten" },
   previewer = true,
-  prompt_title = "AWESOME SESSIONS",
+  prompt_title = "CHARA SESSIONS",
 }
