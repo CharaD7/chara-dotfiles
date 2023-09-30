ecc2e96aef140b73937b3babab88a6d16248ce38
CharaD7 <jijakahn6@gmail.com>
2023-09-30 09:03:57 +0000

    Fixed session lens error

--- nvim/after/plugin/session-lens.lua
+++ nvim/after/plugin/session-lens.lua
@@ -1,11 +1,6 @@
 ---@diagnostic disable: undefined-global
-local status_tel_ok, telescope = pcall(require, 'telescope')
-if not status_tel_ok then return end
-
 local status_ok, session_lens = pcall(require, 'session-lens')
 if not status_ok then return end
-
-telescope.load_extension(session_lens)
 
 session_lens.setup {
   theme = 'ivy',
