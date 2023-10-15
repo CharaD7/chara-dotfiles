cced3001d284b0eddd29af663ad1aff9f791fe96
CharaD7 <jijakahn6@gmail.com>
2023-10-15 17:53:01 +0000

    Reconfigured aurotessions

--- nvim/after/plugin/auto-session.lua
+++ nvim/after/plugin/auto-session.lua
@@ -4,9 +4,8 @@ if not status_ok then return end
 
 auto_session.setup {
   log_level = "info",
-  auto_session_enable_last_session = true,
-  auto_session_enabled = true,
-  auto_save_enabled = true,
+  auto_session_enable_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
+  auto_session_enabled = true, auto_save_enabled = true,
   auto_restore_enabled = true,
   auto_session_suppress_dirs = nil,
 }
