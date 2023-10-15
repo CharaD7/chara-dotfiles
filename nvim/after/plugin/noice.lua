c61636f2fa85a0a30a6669c8ec8e5cb39d2f97d1
CharaD7 <jijakahn6@gmail.com>
2023-10-15 17:53:41 +0000

    Added standalone noice config

---
+++ nvim/after/plugin/noice.lua
@@ -0,0 +1,15 @@
+local status_ok, noice = pcall(require, 'noice')
+if not status_ok then return end
+
+noice.setup {
+  lsp = {
+    override = {
+      ["vim.lsp.textDocument_hover"] = false,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+    -- hover = {
+    --   enabled = false,
+    -- }
+}
