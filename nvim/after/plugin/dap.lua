74560e370b9edf3de4ffd4b9fae0fdc3ad420289
CharaD7 <jijakahn6@gmail.com>
2023-11-08 07:55:10 +0000

    Removed extra comments in dap

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -55,15 +55,7 @@ dapui.setup {
     repl = "r",
     toggle = "t",
   },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
   expand_lines = vim.fn.has "nvim-0.7",
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position. It can be an Int
-  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-  -- Elements are the elements shown in the layout (in order).
   -- Layouts are opened in order so that earlier layouts take priority in window sizing.
   layouts = {
     {
