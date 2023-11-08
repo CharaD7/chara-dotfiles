c6b4ba1320ed64746b9d5f8159e759e48cc33e54
CharaD7 <jijakahn6@gmail.com>
2023-11-08 18:46:34 +0000

    Refactored plugins

--- nvim/lua/chara/plugins.lua
+++ nvim/lua/chara/plugins.lua
@@ -39,48 +39,47 @@ packer.init {
 
 return packer.startup(function(use)
 	-- Packer can manage itself
-	use('wbthomason/packer.nvim')
-	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } } })
-	use('ThePrimeagen/harpoon')
-	use("glepnir/zephyr-nvim")
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("nvim-treesitter/nvim-treesitter-textobjects")
-	use("nvim-treesitter/nvim-treesitter-refactor")
-	use({
+	use 'wbthomason/packer.nvim'
+	use "nvim-telescope/telescope.nvim"
+	use "nvim-lua/popup.nvim"
+	use "nvim-lua/plenary.nvim"
+	use 'ThePrimeagen/harpoon'
+	use "glepnir/zephyr-nvim"
+	use ({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use "nvim-treesitter/nvim-treesitter-textobjects"
+	use "nvim-treesitter/nvim-treesitter-refactor"
+	use ({
 		"romgrk/nvim-treesitter-context",
 		config = function()
 			require("treesitter-context").setup({})
 		end,
-	})                                         -- Shows the current function/class as float window at the top of the window
-	use("theHamsta/nvim-treesitter-commonlisp") -- highlight functions
-	use("nvim-treesitter/playground")
-	use("mbbill/undotree")
-	use("ericglau/vim-reach")
-	use({ "autozimu/LanguageClient-neovim", run = "bash install.sh" })
-	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
-	use("junegunn/fzf")
-	use("sharkdp/fd")
-	use("sheerun/vim-polyglot") -- This is to help with razor files
-	use("nathom/filetype.nvim")
-	use("mhinz/vim-signify")
-	use("MunifTanjim/prettier.nvim")
-	use({ "CRAG666/code_runner.nvim" })
-	use({ "numToStr/Comment.nvim" })
-	use("SmiteshP/nvim-navic")
-	use("ryanoasis/vim-devicons") -- optional, for file icon
-	use({ "akinsho/bufferline.nvim", tag = "*", requires = 'nvim-tree/nvim-web-devicons' })
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons", -- optional, for file icon
-		},
-		tag = "nightly",
+	})
+	use "theHamsta/nvim-treesitter-commonlisp" -- highlight functions
+	use "nvim-treesitter/playground"
+	use "mbbill/undotree"
+	use "ericglau/vim-reach"
+	use ({ "autozimu/LanguageClient-neovim", run = "bash install.sh" })
+	use "rcarriga/nvim-dap-ui"
+	use "junegunn/fzf"
+	use "sharkdp/fd"
+	use "sheerun/vim-polyglot" -- This is to help with razor file
+	use "nathom/filetype.nvim"
+	use "mhinz/vim-signify"
+	use "MunifTanjim/prettier.nvim"
+	use "CRAG666/code_runner.nvim"
+	use "numToStr/Comment.nvim"
+	use "SmiteshP/nvim-navic"
+	use "ryanoasis/vim-devicons" -- optional, for file icon
+	use "kyazdani42/nvim-web-devicons"
+	use 'nvim-tree/nvim-web-devicons'
+	use ({ "akinsho/bufferline.nvim", tag = "*" })
+	use ({ "kyazdani42/nvim-tree.lua", tag = "nightly",
 		config = function()
 			require("nvim-tree").setup({})
 		end,
 	})
 	-- for resolving git merge conflicts directly in neovim
-	use({
+	use ({
 		'akinsho/git-conflict.nvim',
 		tag = "*",
 		config = function()
@@ -88,74 +87,67 @@ return packer.startup(function(use)
 		end,
 	})
 	-- Markdown preview
-	use({
+	use ({
 		"iamcco/markdown-preview.nvim",
 		run = function() vim.fn["mkdp#util#install"]() end,
 	})
 	-- git related
-	use("rmagatti/auto-session")
-	use("rmagatti/session-lens")
-	use({ "lewis6991/gitsigns.nvim" })
-	use("tpope/vim-repeat")
+	use "rmagatti/auto-session"
+	use "rmagatti/session-lens"
+	use "lewis6991/gitsigns.nvim"
+	use "tpope/vim-repeat"
 	-- use('teal-language/vim-teal')
-	use("tpope/vim-endwise")
-	use("f-person/git-blame.nvim") -- show git message
-	use("andymass/vim-matchup")   -- Provides language-specific % style pair and tuple matching, highlighting, and text-objects
-	use({
+	use "tpope/vim-endwise"
+	use "f-person/git-blame.nvim" -- show git message
+	use "andymass/vim-matchup"   -- Provides language-specific % style pair and tuple matching, highlighting, and text-objects
+	use ({
 		"beauwilliams/focus.nvim",
 		config = function()
 			require("focus").setup()
 		end,
 	})                                -- auto screen resizer
-	use("Xuyuanp/scrollbar.nvim")     -- Scrollbar
-	use("norcalli/nvim-colorizer.lua") -- Color value highlighting
-	use({
+	use "Xuyuanp/scrollbar.nvim"     -- Scrollbar
+	use "norcalli/nvim-colorizer.lua" -- Color value highlighting
+	use ({
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
 		end
 	})
 	-- navigation finder operator
-	use("haya14busa/vim-asterisk")
-	use("nvim-lualine/lualine.nvim")
-	use("mg979/vim-visual-multi")
-	use({
+	use "haya14busa/vim-asterisk"
+	use "nvim-lualine/lualine.nvim"
+	use "mg979/vim-visual-multi"
+	use ({
 		"phaazon/hop.nvim",
 		branch = "v2", -- optional but strongly recommended
 		config = function()
 			require("hop").setup()
 		end,
 	})
-	use("ggandor/lightspeed.nvim")
-	use({
+	use "ggandor/lightspeed.nvim"
+	use ({
 		"ahmedkhalf/project.nvim",
 		config = function()
 			require("project_nvim").setup({})
 			require("telescope").load_extension("projects")
 		end,
 	})
-	use({
-		"MunifTanjim/nui.nvim",
-	})
-	use("christoomey/vim-tmux-navigator")
+	use "MunifTanjim/nui.nvim"
+	use "christoomey/vim-tmux-navigator"
 	-- Noice
-	use({
+	use ({
 		"folke/noice.nvim",
 		event = "VimEnter",
-		-- event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
 	})
-	use({
+	use ({
 		"max397574/colortils.nvim",
 		cmd = "Colortils",
 		config = function()
 			require("colortils").setup()
 		end,
 	})
-	use("simrat39/rust-tools.nvim")
-	use({
+	use "simrat39/rust-tools.nvim"
+	use ({
 			'glepnir/nerdicons.nvim',
 			cmd = 'NerdIcons',
 			config = function()
@@ -163,86 +155,76 @@ return packer.startup(function(use)
 		end
 	})
 	-- Java support
-	use("mfussenegger/nvim-jdtls")
-	use("artur-shaik/jc.nvim")
+	use "mfussenegger/nvim-jdtls"
+	use "mfussenegger/nvim-dap"
+	use "artur-shaik/jc.nvim"
 	-- Grammar suggestions load_extension
-	use("neovim/nvim-lspconfig")
-	use("RRethy/vim-illuminate")
-	use("nvim-telescope/telescope-media-files.nvim")
-	use("nvim-telescope/telescope-file-browser.nvim")
-	use("xiyaowong/nvim-transparent")
-	use({ "diepm/vim-rest-console", ft = { "rest" } }) -- rest live testing
-	use({
-		"nvim-neotest/neotest",
-		requires = {
-			"nvim-neotest/neotest-python",
-			"nvim-neotest/neotest-plenary",
-			"nvim-neotest/neotest-go",
-			"haydenmeade/neotest-jest",
-			"nvim-neotest/neotest-vim-test",
-		}
-	})
-	use("folke/neodev.nvim")
-	use("hrsh7th/nvim-cmp")
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
-	use({
-		"hrsh7th/cmp-nvim-lsp",
-		requires = {
-			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "ray-x/cmp-treesitter" },
-			{ "hrsh7th/cmp-calc" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			{ "hrsh7th/cmp-emoji" },
-			{ "tzachar/cmp-tabnine", run = "./install.sh" },
-		},
-	})
-	use("goolord/alpha-nvim")
-	use("xiyaowong/telescope-emoji.nvim") -- allow looking up and using emojies inside out files
+	use "neovim/nvim-lspconfig"
+	use "RRethy/vim-illuminate"
+	use "nvim-telescope/telescope-media-files.nvim"
+	use "nvim-telescope/telescope-file-browser.nvim"
+	use "xiyaowong/nvim-transparent"
+	use ({ "diepm/vim-rest-console", ft = { "rest" } }) -- rest live testing
+	use "nvim-neotest/neotest"
+	use "nvim-neotest/neotest-python"
+	use "nvim-neotest/neotest-plenary"
+	use "nvim-neotest/neotest-go"
+	use "haydenmeade/neotest-jest"
+	use "nvim-neotest/neotest-vim-test"
+	use "folke/neodev.nvim"
+	use "L3MON4D3/LuaSnip"
+	use "saadparwaiz1/cmp_luasnip"
+	use "hrsh7th/nvim-cmp"
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-path"
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/cmp-calc"
+	use "hrsh7th/cmp-nvim-lua"
+	use "hrsh7th/cmp-cmdline"
+	use "hrsh7th/cmp-emoji"
+	use "ray-x/cmp-treesitter"
+	use ({ "tzachar/cmp-tabnine", run = "./install.sh" })
+	use "goolord/alpha-nvim"
+	use "xiyaowong/telescope-emoji.nvim" -- allow looking up and using emojies inside out files
 	-- Grammar tips
-	use("folke/lsp-trouble.nvim")
-	use("onsails/lspkind-nvim")
-	use("liuchengxu/vista.vim")
-	use("pantharshit00/vim-prisma") -- prisma support
-	use({
+	use "folke/lsp-trouble.nvim"
+	use "onsails/lspkind-nvim"
+	use "liuchengxu/vista.vim"
+	use "pantharshit00/vim-prisma" -- prisma support
+	use ({
 		"ray-x/navigator.lua",
-		requires = {
-			{ "ray-x/guihua.lua", run = "cd lua/fzy && make" },
-		},
 		config = function()
 			require("navigator").setup()
 		end,
 	})
-	use("mtth/scratch.vim") -- For taking notes (Uses 'gs' to invoke command)
-	use({
+	use ({ "ray-x/guihua.lua", run = "cd lua/fzy && make" })
+	use "mtth/scratch.vim" -- For taking notes (Uses 'gs' to invoke command)
+	use ({
 		"weilbith/nvim-code-action-menu",
 		cmd = "CodeActionMenu",
 	})
-	use({
+	use ({
 		"glepnir/lspsaga.nvim",
 		branch = "main",
 		config = function()
 			require('lspsaga').setup({})
 		end
 	})
-	-- use({ "glepnir/lspsaga.nvim", branch = "main", commit = "b7b4777" })
-	use("tomlion/vim-solidity")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("kosayoda/nvim-lightbulb")
+	use "tomlion/vim-solidity"
+	use "williamboman/mason.nvim"
+	use "williamboman/mason-lspconfig.nvim"
+	use "kosayoda/nvim-lightbulb"
 	-- use("jose-elias-alvarez/null-ls.nvim") -- snippet related use 'hrsh7th/vim-vsnip'
-	use("hrsh7th/cmp-cmdline")
-	use("rafamadriz/friendly-snippets")
+	use "rafamadriz/friendly-snippets"
 	-- Easy to operate
-	use("tpope/vim-eunuch")
-	use("gennaro-tedesco/nvim-peekup") -- View historical copy and delete registers, shortcut keys ""
-	use("voldikss/vim-translator")    -- npm install fanyi -g (install translation)
-	use("windwp/nvim-autopairs")      -- Automatic symbol matching
-	use("windwp/nvim-ts-autotag")
-	use("chaoren/vim-wordmotion")
+	use "tpope/vim-eunuch"
+	use "gennaro-tedesco/nvim-peekup" -- View historical copy and delete registers, shortcut keys ""
+	use "voldikss/vim-translator"    -- npm install fanyi -g (install translation)
+	use "windwp/nvim-autopairs"      -- Automatic symbol matching
+	use "windwp/nvim-ts-autotag"
+	use "chaoren/vim-wordmotion"
 	-- for saving code snapshots
-	use({
+	use ({
 		"krivahtoo/silicon.nvim",
 		run = './install.sh',
 		config = function()
@@ -255,46 +237,28 @@ return packer.startup(function(use)
 			}
 		end,
 	})
-	use("ur4ltz/surround.nvim")
-	use("folke/which-key.nvim") -- hint leader button
+	use "ur4ltz/surround.nvim"
+	use "folke/which-key.nvim" -- hint leader button
 	-- intuitive toggleterm
-	use { "akinsho/toggleterm.nvim", tag = '*' }
-	use("sindrets/diffview.nvim") -- diff compare
-	use({
+	use ({ "akinsho/toggleterm.nvim", tag = '*' })
+	use "sindrets/diffview.nvim" -- diff compare
+	use ({
 		"SmiteshP/nvim-gps",
 		config = function()
 			require("nvim-gps").setup()
 		end,
 	})
-	use("p00f/nvim-ts-rainbow") -- Rainbow matching
-	use("folke/todo-comments.nvim")
-	use("tpope/vim-fugitive")
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			-- Snippet Collection (Optional)
-			{ 'rafamadriz/friendly-snippets' },
-		}
-	}
-	use("ThePrimeagen/vim-be-good")
-	use({ "rcarriga/nvim-notify", config = 'vim.notify = require("notify")' })
-	use({ "michaelb/sniprun", run = "bash ./install.sh" })
-	use('anuvyklack/pretty-fold.nvim')
+	use "p00f/nvim-ts-rainbow" -- Rainbow matching
+	use "folke/todo-comments.nvim"
+	use "tpope/vim-fugitive"
+	use 'VonHeikemen/lsp-zero.nvim'
+	use "ThePrimeagen/vim-be-good"
+	use ({ "rcarriga/nvim-notify", config = 'vim.notify = require("notify")' })
+	use ({ "michaelb/sniprun", run = "bash ./install.sh" })
+	use 'anuvyklack/pretty-fold.nvim'
 
 	if PACKER_BOOTSTRAP then
 		require('packer').sync()
 	end
 end)
+
