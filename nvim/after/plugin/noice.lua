ebe26f46302d011d96ebb501a7b19c4a87078da2
CharaD7 <jijakahn6@gmail.com>
2023-11-08 08:08:31 +0000

    Added extra configs for noice

--- nvim/after/plugin/noice.lua
+++ nvim/after/plugin/noice.lua
@@ -3,14 +3,31 @@ if not status_ok then return end
 
 noice.setup {
   lsp = {
+    cmdline = {
+      enabled = true,
+      view = "cmdline_popup",
+    },
+    hover = {
+      enabled = true,
+    },
+    message = {
+      enabled = true,
+      view = "notify",
+    },
     override = {
       ["vim.lsp.textDocument_hover"] = false,
       ["vim.lsp.util.stylize_markdown"] = true,
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["cmp.entry.get_documentation"] = true,
     },
-    hover = {
-      enabled = false,
-    }
+    popupmenu = {
+      enabled = true,
+      backend = "nui"
+    },
+    preset = {
+      bottom_search = true,
+      command_pallete = true,
+      lsp_doc_border = false
+    },
   }
 }
