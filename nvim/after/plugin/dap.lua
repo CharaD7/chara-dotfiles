6a48d3a86e8710de6c24f630ba9072d0ea9213fb
CharaD7 <jijakahn6@gmail.com>
2023-11-08 07:48:37 +0000

    Added dap config for rust

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -20,6 +20,17 @@ dap.adapters.codelldb = {
   },
 }
 
+dap.configurations.rust = {
+  name = "Launch file",
+  type = "codelldb",
+  request = "launch",
+  program = function ()
+    return vim.fn.input('Path to executable: ', vim.fn.getcwd()..'/', 'file')
+  end,
+  cwd = "${workspaceFolder}",
+  stopOnEntry = false,
+}
+
 -- dapui.setup()
 dapui.setup {
   icons = { expanded = "▾", collapsed = "▸" },
