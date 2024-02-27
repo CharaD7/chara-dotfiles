caa85f9685d233a7da5bac418b48878f7e26ad8f
CharaD7 <jijakahn6@gmail.com>
2024-02-27 21:45:09 +0000

    Added more plugin configs for lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,19 +1,93 @@
 return {
   -- Mason
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "stylua",
-      "luacheck",
-      "shfmt",
-      "tailwindcss-language-server",
-      "typescript-language-server",
-      "css-lsp",
-      "shellcheck",
-      "vls",
-      "clangd",
-      "marksman",
-      "gopls",
-    })
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "luacheck",
+        "shfmt",
+        "eslint-lsp",
+        "csharp-language-server",
+        "htmx-lsp",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "pylyzer",
+        "css-lsp",
+        "emmet-ls",
+        "shellcheck",
+        "vls",
+        "clangd",
+        "marksman",
+        "gopls",
+        "prettier",
+      })
+    end,
+  },
+  -- LSPconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        -- Html
+        html = {
+          filetpyes = {
+            "html",
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx"
+          }
+        },
+        -- Emmet
+        emmet_ls = {
+          init_options = {
+            html = {
+              options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+        -- CSS
+        cssls = {},
+        -- eslint
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectory = { mode = "auto" },
+          },
+        },
+      },
+    },
+  },
+  -- Null-ls
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.prettierd)
+    end,
+  },
+  -- Color picker
+  {
+    "uga-rosa/ccc.nvim",
+    opts = {},
+    cmd = {
+      "CccPick",
+      "CccConvert",
+      "CccHighlighterEnable",
+      "CccHighlighterDisable",
+      "CccHighlighterToggle",
+    },
+    keys = {
+      { "<leader>zp", "<cmd>CccPick<cr>", desc = "Pick" },
+      { "<leader>zc", "<cmd>CccConvert<cr>", desc = "Convert" },
+      { "<leader>zh", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle Highlighter" },
+    },
+  },
 }
