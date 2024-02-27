bb5cdadf5a5e740a3ba6c28ce890c7380b6799d0
CharaD7 <jijakahn6@gmail.com>
2024-02-27 21:49:25 +0000

    Add dunst daemon to startup

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+userName=$(echo $USER)
+
 xrdb merge ~/.Xresources
 xrandr --output HDMI-0 --auto --left-of eDP-1-1
+
+xpad -h &
+
+chown $userName:$userName /sys/class/backlight/intel_backlight/brightness
 # compton &
 xset r rate 200 50 &
 picom &
 
 wal &
-fetch &
+dunst & # Add notification daemon to startup
 
-~/.config/dwm/scripts/bar &
+~/.config/chadwm/scripts/bar &
+dwm 2> ~/dwm.log
 
 while type dwm >/dev/null; do dwm && continue || break; done
