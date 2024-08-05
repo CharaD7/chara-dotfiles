43de933fac208af7a94814cafa315f97b8df71a8
CharaD7 <jijakahn6@gmail.com>
2024-08-05 10:38:40 +0000

    Updated nvim bufferline config
    - Enabled lazy
    - Customize bufferline config to suit my desire

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,60 +4,74 @@ for _, num in pairs(numbers) do
 	vim.keymap.set("n", "<leader>" .. num, "<cmd>BufferLineGoToBuffer " .. num .. "<CR>")
 end
 
+local bufferline = require "bufferline"
+
 return {
   "akinsho/bufferline.nvim",
   version = "*",
+  lazy = true,
   dependencies = "nvim-tree/nvim-web-devicons",
-  options = {
-    numbers = function(opts)
-			return string.format("%s", opts.raise(opts.ordinal))
-			-- return string.format('%s.%s', opts.ordinal, opts.raise(opts.id))
-		end,
-    indicator = {
-      style = "icon",
+  bufferline.setup({
+    options = {
+      mode = "buffers", -- tabs or buffers
+      numbers = function(opts)
+        return string.format("%s", opts.raise(opts.ordinal))
+      end,
+      indicator = {
+        style = "icon",
+      },
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local s = " "
+        for e, n in pairs(diagnostics_dict) do
+          local sym = e == "error" and "   " or (e == "warning" and "   " or "   ")
+          s = s .. n .. sym
+        end
+        return s
+      end,
+      diagnostics = "nvim_lsp",
+      diagnostics_update_in_insert = true,
+      color_icons = true,
+      sort_by = "directory", -- 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+      show_tab_indicators = true,
+      separator_style = "slope",
+      custom_areas = {
+        right = function()
+          local result = {}
+          local seve = vim.diagnostic.severity
+          local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+          local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+          local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+          local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+          local icons = require "chara.icons"
+
+          if error ~= 0 then
+            table.insert(result, { text = " " .. icons.diagnostics.Error .. error, guifg = "#EC5241" })
+          end
+
+          if warning ~= 0 then
+            table.insert(result, { text = " " .. icons.diagnostics.Warning .. warning, guifg = "#EFB839" })
+          end
+
+          if hint ~= 0 then
+            table.insert(result, { text = " " .. icons.diagnostics.Hint .. hint, guifg = "#A3BA5E" })
+          end
+
+          if info ~= 0 then
+            table.insert(result, { text = " " .. icons.diagnostics.Information .. info, guifg = "#7EA9A7" })
+          end
+          return result
+        end,
+      },
+      always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          separator = true -- use a "true" to enable the default, or set your own character
+        }
+      }
     },
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-			local s = " "
-			for e, n in pairs(diagnostics_dict) do
-				local sym = e == "error" and "  " or (e == "warning" and "  " or "  ")
-				s = s .. n .. sym
-			end
-			return s
-		end,
-    diagnostics = "nvim_lsp",
-    color_icons = true,
-    sort_by = "relative_directory",
-    show_tab_indicators = true,
-    separator_style = "thin",
-    custom_areas = {
-			right = function()
-				local result = {}
-				local seve = vim.diagnostic.severity
-				local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
-				local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
-				local info = #vim.diagnostic.get(0, { severity = seve.INFO })
-				local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
-
-        local icons = require "chara.icons"
-
-				if error ~= 0 then
-					table.insert(result, { text = " " .. icons.diagnostics.Error .. error, guifg = "#EC5241" })
-				end
-
-				if warning ~= 0 then
-					table.insert(result, { text = " " .. icons.diagnostics.Warning .. warning, guifg = "#EFB839" })
-				end
-
-				if hint ~= 0 then
-					table.insert(result, { text = " " .. icons.diagnostics.Hint .. hint, guifg = "#A3BA5E" })
-				end
-
-				if info ~= 0 then
-					table.insert(result, { text = " " .. icons.diagnostics.Information .. info, guifg = "#7EA9A7" })
-				end
-				return result
-			end,
-		},
-    always_show_bufferline = false,
-  },
+  })
 }
